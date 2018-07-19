2b299c9bee023d81c6943c585ae5cdbfc7e0b880
zhangbin-eos <zhangbin.eos@foxmail.com>
2018-07-19 15:20:53 +0800

    add remove_loggin

--- test_code/remove_loggin/expect_for_telnet.py
+++ test_code/remove_loggin/expect_for_telnet.py
@@ -1,42 +1,33 @@
-import telnetlib
-import sys
-
 def do_telnet(Host, username, password, finish, commands):  
-
+    import telnetlib  
     '''''Telnet远程登录：Windows客户端连接Linux服务器'''  
-
+   
     # 连接Telnet服务器  
     tn = telnetlib.Telnet(Host, port=23, timeout=10)  
-    tn.set_debuglevel(0)  
+    tn.set_debuglevel(2)  
     # 输入登录用户名  
     tn.read_until(b'ogin:')  
     tn.write(username.encode())  
-
+      
     # 输入登录密码  
     tn.read_until(b'assword:')  
     tn.write(password.encode())  
-
+        
     # 登录完毕后执行命令  
     tn.read_until(finish.encode())  
     for command in commands:  
         tn.write(command.encode())
-        
-    tn.read_until(finish.encode())  
+      
     #执行完毕后，终止Telnet连接（或输入exit退出）  
+    tn.read_until(finish.encode())
+    tn.read_until(finish.encode()) 
     tn.close() # tn.write('exit\n')  
   
-if __name__=='__main__':
-    # 配置选项
-    if len(sys.argv)<3:
-        print("Usage:",sys.argv[0],"[ip] [cmdfile]")
-        sys.exit();
-    
-    Host =  sys.argv[1]# Telnet服务器IP  
+if __name__=='__main__':  
+     # 配置选项  
+    Host = '192.168.20.135' # Telnet服务器IP  
     username = 'root\n'   # 登录用户名  
     password = '\n'  # 登录密码  
-    finish = ':~#'      # 命令提示符
-    #read cmd shell file
-    commands = ['echo "test"\n','ls -l / \n']  
-
+    finish = ':~#'      # 命令提示符  
+    commands = ['echo "test"\n']  
     do_telnet(Host, username, password, finish, commands) 
-    sys.exit();
---
+++ test_code/remove_loggin/setup_nandflash.cmd
@@ -0,0 +1,19 @@
+cd /home/root
+tftp -g -r nandflash.tar 192.168.20.35
+rm /nandflash/* -rf
+rm /nandflash/.[!.]* -rf
+
+cd /nandflash
+tar -xf /home/root/nandflash.tar
+sleep 1
+
+echo "lig_ifcfg eth0 IP 192.168.20.135 " > /nandflash/run.d/U02zbconfig.sh
+echo "lig_ifcfg eth0 MAC 00:50:56:27:BB:CC " >> /nandflash/run.d/U02zbconfig.sh
+echo "syslog_stop -s && syslog_start -s 192.168.20.127" >> /nandflash/run.d/U02zbconfig.sh
+
+echo "rm /nandflash/run.d/U02zbconfig.sh " >> /nandflash/run.d/U02zbconfig.sh
+echo "34" > /nandflash/lib/.liblig_matrix.debug
+
+reboot
+exit
+
